f26d2b6407cbdd774032d5c5848dc59211942194
Y Q N O <79216939+Yqno@users.noreply.github.com>
2022-12-17 09:04:35 +0100

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,6 @@
+let fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
+
+console.log(fruits[0]); // Ausgabe: "Apple"
+console.log(fruits[1]); // Ausgabe: "Banana"
+console.log(fruits[2]); // Ausgabe: "Orange"
+console.log(fruits[3]); // Ausgabe: "Mango"
